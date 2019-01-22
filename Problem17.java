fd02bd8cb678a7bba944858438e6beefba52ccf1
YanshuGupta <35223814+YanshuGupta@users.noreply.github.com>
2019-01-22 17:57:13 +0530

    Add files via upload

---
+++ Problem17.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem17 {
+
+	public static void main(String[] args) {
+		//numerology number
+		int count[] = {1,2,3,4,5,8,3,5,1,1,2,3,4,5,7,8,1,2,3,4,6,6,6,5,1,7};
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		
+	}
+
+}
