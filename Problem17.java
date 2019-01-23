8809c3be01b5b46e620b2350835d6ed79618d7c3
YanshuGupta <35223814+YanshuGupta@users.noreply.github.com>
2019-01-23 09:15:33 +0530

    Update Problem17.java

--- Problem17.java
+++ Problem17.java
@@ -7,7 +7,22 @@ public class Problem17 {
 		int count[] = {1,2,3,4,5,8,3,5,1,1,2,3,4,5,7,8,1,2,3,4,6,6,6,5,1,7};
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
-		
+		int c=0;
+		for(int i=0;i<s.length();i++) {
+			if(Character.toUpperCase(s.charAt(i)) >= 'A' && Character.toUpperCase(s.charAt(i)) <= 'Z') {
+				c += count[Character.toUpperCase(s.charAt(i))-65];
+			}
+		}
+		if(c==0) {
+			c=0;
+		}
+		else if(c%9==0) {
+			c=9;
+		}
+		else {
+			c=c%9;
+		}
+		System.out.println("Numerology count is ="+c);
 	}
 
 }
